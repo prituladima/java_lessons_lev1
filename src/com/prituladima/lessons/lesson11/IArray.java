9a05d62f10290ce49e19d4b17d1f45c767e7fb5d
dmprytul <z6ghdi4ogo75ojnesjqicnfhn5wz2v6xvo42lmq6scttwiqntxhq>
2021-04-02 10:50:17 +0300

    hw

---
+++ src/com/prituladima/lessons/lesson11/IArray.java
@@ -0,0 +1,15 @@
+package com.prituladima.lessons.lesson11;
+
+public interface IArray {
+
+    void add(int val);
+
+    int get(int ind);
+
+    void set(int ind, int val);
+
+    void insert(int ind, int val);
+
+    void remove(int ind);
+
+}
---
+++ src/com/prituladima/lessons/lesson11/IDeque.java
@@ -0,0 +1,21 @@
+package com.prituladima.lessons.lesson11;
+
+public interface IDeque {
+
+    int length();
+
+    boolean isEmpty();
+
+    void addFirst(int val);
+
+    void addLast(int val);
+
+    int getFirst();
+
+    int getLast();
+
+    int removeFirst();
+
+    int removeLast();
+
+}
